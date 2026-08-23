f9306930c5f67d5945fbfb6b93b44264481510fd
Shivam-Gupta14 <gupta.shivam07.14@gmail.com>
2026-08-23 22:02:53 +0530

    LC-diameter-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ dp-on-trees/diameter-of-binary-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftH = height(node.left);
+        int rightH = height(node.right);
+
+        diameter = Math.max(diameter, leftH + rightH);
+
+        return Math.max(leftH, rightH) + 1;
+    }
+}
